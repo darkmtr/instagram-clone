e7c0f12c510e2730241777c68e31b5d3a3c13bfa
David John <davidkjohn01@gmail.com>
2020-05-15 12:37:14 +0530

    added emoji picker for post

--- frontend/src/Components/NewPost.jsx
+++ frontend/src/Components/NewPost.jsx
@@ -1,7 +1,12 @@
 import React, { useState } from 'react';
 import styled from 'styled-components';
+import 'emoji-mart/css/emoji-mart.css';
+import { Picker } from 'emoji-mart';
+import { useRef } from 'react';
+import { uploadImage } from '../utils/imageUpload';
 
 const Container = styled.div`
+  position: relative;
   background: #fff;
   height: 15rem;
   width: 100rem;
@@ -34,23 +39,104 @@ const PostInput = styled.input`
   }
 `;
 
+const PickerIcon = styled.i`
+  position: absolute;
+  top: 2rem;
+  right: 2rem;
+  font-size: 2rem;
+  z-index: 10;
+  color: #e91f35;
+  cursor: pointer;
+
+  &:hover {
+    color: #eeb32e;
+  }
+`;
+
+const ImagePicker = styled.div`
+  padding: 1rem 2rem;
+`;
+
+const Icon = styled.i`
+  font-size: 2.5rem;
+  cursor: pointer;
+`;
+
+const ImageFile = styled.input`
+  display: none;
+`;
+
 export default function NewPost() {
-  const [image, setImage] = useState(null);
+  const [values, setValues] = useState({
+    image: '',
+    desc: '',
+    imageName: '',
+  });
+
+  const inpRef = useRef(null);
+
+  const [showPicker, setShowPicker] = useState(false);
+
+  const handleImageChange = async (e) => {
+    const file = e.target.files[0];
+    if (!file) return;
+    const data = await uploadImage(file);
+    setValues({
+      ...values,
+      imageName: file.name,
+      image: data.url,
+    });
+  };
 
   const handleChange = (e) => {
-    const file = e.target.files[0];
-    if (file) {
-      const reader = new FileReader();
-      reader.readAsDataURL(file);
-      reader.onload = function (e) {
-        setImage(this.result);
-      };
-    }
+    setValues({
+      ...values,
+      [e.target.name]: e.target.value,
+    });
+  };
+
+  const addEmoji = (e) => {
+    console.log(e.native);
+    setValues({
+      ...values,
+      desc: `${values.desc} ${e.native}`,
+    });
+  };
+
+  const handleIconClick = (e) => {
+    inpRef.current.click();
   };
 
   return (
     <Container>
-      <PostInput placeholder='Tell us about the photo' />
+      <PostInput
+        value={values.desc}
+        name='desc'
+        type='text'
+        onChange={handleChange}
+        placeholder='Tell us about the photo'
+      />
+      <PickerIcon
+        className='far fa-laugh'
+        onClick={() => setShowPicker(!showPicker)}
+      ></PickerIcon>
+      {showPicker && (
+        <Picker
+          onSelect={addEmoji}
+          title='Pick your emoji…'
+          emoji='point_up'
+          style={{ position: 'absolute', top: '5rem', right: '2rem' }}
+        />
+      )}
+      <ImagePicker>
+        <Icon onClick={handleIconClick} className='fas fa-images'></Icon>
+        <ImageFile ref={inpRef} type='file' onChange={handleImageChange} />
+        {values.imageName && (
+          <span style={{ marginLeft: '5px' }}>
+            Image selected : {values.imageName}
+          </span>
+        )}
+      </ImagePicker>
     </Container>
   );
 }
